ed9bf838d193763e8c2654089c5546aac690a919
zoucongchao <798422668@qq.com>
2019-03-09 15:46:09 +0800

    startA

---
+++ java/CommentTest.java
@@ -0,0 +1,14 @@
+
+//public类名称必须与文件名一致
+public class CommentTest 
+{
+	/*
+	大括号里的是类体
+	java版
+	*/
+	public static void main(String[] args)
+	{
+		//单行注释
+		System.out.println("hello world!");
+	}
+}
